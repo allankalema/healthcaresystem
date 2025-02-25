49c64e86435df7fca06f2823e02058fa38cf8986
allankalema4 <allankalema4@gmail.com>
2025-02-25 10:23:40 +0300

    rectified the admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 from .models import User
-
 class UserAdmin(BaseUserAdmin):
     list_display = ('username', 'first_name', 'last_name', 'email', 'is_doctor', 'is_patient', 'is_hospital', 'is_staff', 'is_superuser')
     list_filter = ('is_doctor', 'is_patient', 'is_hospital', 'is_staff', 'is_superuser', 'marital_status')
@@ -13,7 +12,7 @@ class UserAdmin(BaseUserAdmin):
     fieldsets = (
         (_("Personal Information"), {'fields': ('username', 'first_name', 'last_name', 'email', 'date_of_birth', 'nin', 'marital_status', 'contact')}),
         (_("Permissions"), {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_doctor', 'is_patient', 'is_hospital', 'groups', 'user_permissions')}),
-        (_("Important Dates"), {'fields': ('last_login', 'created_at', 'updated_at')}),
+        (_("Important Dates"), {'fields': ('last_login', 'created_at')}),  # Removed 'updated_at'
     )
 
     add_fieldsets = (

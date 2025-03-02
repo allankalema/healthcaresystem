e6bff41b5e86969ba23be15f17d7763223810b24
allankalema4 <allankalema4@gmail.com>
2025-03-02 22:43:24 +0300

    removed the current table for doctors

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
-from .models import User, Doctor, Specialization
+from .models import User
 
 class UserAdmin(BaseUserAdmin):
     list_display = ('username', 'first_name', 'last_name', 'email', 'is_doctor', 'is_patient', 'is_hospital', 'is_staff', 'is_superuser')
@@ -23,20 +23,7 @@ class UserAdmin(BaseUserAdmin):
         }),
     )
 
-class DoctorAdmin(admin.ModelAdmin):
-    list_display = ('user', 'license_number', 'education_level', 'rank', 'display_specializations')
-    search_fields = ('user__username', 'user__first_name', 'user__last_name', 'license_number')
-    list_filter = ('education_level', 'rank', 'specializations')
-    filter_horizontal = ('specializations',)
 
-    def display_specializations(self, obj):
-        return ", ".join([s.name for s in obj.specializations.all()])
-    display_specializations.short_description = "Specializations"
-
-class SpecializationAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Doctor, DoctorAdmin)
-admin.site.register(Specialization, SpecializationAdmin)
+

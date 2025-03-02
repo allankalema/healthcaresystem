4fed958347633a8cb639fec7213fa678b64382fa
allankalema4 <allankalema4@gmail.com>
2025-03-02 14:50:50 +0300

    forgot password templates done

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,10 @@ from .views import *
 
 urlpatterns = [
     path('',  base, name ='base' ),
+    path('enter_code/',  enter_code, name ='enter_code' ),
     path('login/',  login, name ='login' ),
+    path('forgot_password/',  forgot_password, name ='forgot_password' ),
+    path('reset_password/',  reset_password, name ='reset_password' ),
     
     
     
--- accounts/views.py
+++ accounts/views.py
@@ -5,3 +5,12 @@ def base(request):
 
 def login(request):
     return render(request, 'accounts/login.html') 
+
+def enter_code(request):
+    return render(request, 'accounts/enter_code.html') 
+
+def forgot_password(request):
+    return render(request, 'accounts/forgot_password.html') 
+
+def reset_password(request):
+    return render(request, 'accounts/reset_password.html') 

b903f8a5ba760a3a4892b6e324e33be63a03916a
allankalema4 <allankalema4@gmail.com>
2025-03-03 22:54:14 +0300

    viewinf details for prescription and the antental card

--- antenental/urls.py
+++ antenental/urls.py
@@ -1,10 +1,7 @@
-
 from django.urls import path
-from .views import *
-
+from . import views
 
 urlpatterns = [
-    
-    
-    
+    path('antenatal-card/<int:card_id>/', views.antenatal_card_details, name='antenatal_card_details'),
+    path('prescription/<int:prescription_id>/', views.prescription_details, name='prescription_details'),
 ]
--- antenental/views.py
+++ antenental/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import AntenatalCard, Prescription
+from accounts.decorators import *
 
-# Create your views here.
+@login_required
+@patient
+def antenatal_card_details(request, card_id):
+    antenatal_card = get_object_or_404(AntenatalCard, id=card_id, user=request.user)
+    return render(request, 'antenatal/antenatal_card_details.html', {'antenatal_card': antenatal_card})
+
+
+@login_required
+@patient
+def prescription_details(request, prescription_id):
+    prescription = get_object_or_404(Prescription, id=prescription_id, patient=request.user)
+    return render(request, 'antenatal/prescription_details.html', {'prescription': prescription})

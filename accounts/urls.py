ac0f5d30696035821965bc18f129db2c1019d1b4
allankalema4 <allankalema4@gmail.com>
2025-03-03 08:18:37 +0300

    login implemented

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,9 @@ from .views import *
 
 
 urlpatterns = [
-    path('',  base, name ='base' ),
+    path('',  base, name ='home' ),
     path('enter_code/',  enter_code, name ='enter_code' ),
-    path('login/',  login, name ='login' ),
+    path('login/',  login_view, name ='login' ),
     path('forgot_password/',  forgot_password, name ='forgot_password' ),
     path('reset_password/',  reset_password, name ='reset_password' ),
     
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+
 
 def base(request):
     return render(request, 'home.html') 
 
-def login(request):
-    return render(request, 'accounts/login.html') 
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login successful! Welcome back.")
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
+
+    return render(request, "accounts/login.html")
+
 
 def enter_code(request):
     return render(request, 'accounts/enter_code.html') 

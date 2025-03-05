bb855863020adf9478ff08f6a0925d3ee7f45ebc
allankalema4 <allankalema4@gmail.com>
2025-03-06 02:04:11 +0300

    form

--- antenental/forms.py
+++ antenental/forms.py
@@ -5,13 +5,16 @@ class PreviousObstetricHistoryForm(forms.ModelForm):
     class Meta:
         model = PreviousObstetricHistory
         fields = '__all__'
+        exclude = ['antenatal_card', 'recorded_by']
 
 class AntenatalProgressExaminationForm(forms.ModelForm):
     class Meta:
         model = AntenatalProgressExamination
         fields = '__all__'
+        exclude = ['antenatal_card', 'recorded_by']
 
 class UltrasoundReportForm(forms.ModelForm):
     class Meta:
         model = UltrasoundReport
         fields = '__all__'
+        exclude = ['antenatal_card', 'recorded_by']

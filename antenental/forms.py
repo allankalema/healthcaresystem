53f6683553b5ff5ca477f87fb65ff9a986dbac3b
allankalema4 <allankalema4@gmail.com>
2025-03-06 00:17:02 +0300

    viewing the antental card

--- antenental/forms.py
+++ antenental/forms.py
@@ -1,25 +1,20 @@
 from django import forms
-from .models import *
-from accounts.models import *
+from .models import PreviousObstetricHistory, AntenatalProgressExamination, UltrasoundReport
 
-class AntenatalCardForm(forms.ModelForm):
+class PreviousObstetricHistoryForm(forms.ModelForm):
     class Meta:
-        model = AntenatalCard
-        fields = [
-            'health_unit', 'reg_no', 'nin', 'phone_no', 'age', 'village', 'parish', 'sub_county', 'district',
-            'occupation', 'religion', 'education_level', 'tribe', 'marital_status', 'next_of_kin_name',
-            'next_of_kin_phone', 'next_of_kin_relationship', 'next_of_kin_address', 'gravida', 'para', 'abortions',
-            'presenting_complaints', 'first_day_of_lnmp', 'edd', 'weeks_of_amenorrhea', 'complications_of_pregnancy',
-            'hospitalization', 'hospitalization_reason', 'next_visit'
-        ]
-        widgets = {
-            'nin': forms.TextInput(attrs={'readonly': 'readonly'}),
-            'phone_no': forms.TextInput(attrs={'readonly': 'readonly'}),
-            'age': forms.NumberInput(attrs={'readonly': 'readonly'}),
-            'village': forms.TextInput(attrs={'readonly': 'readonly'}),
-            'parish': forms.TextInput(attrs={'readonly': 'readonly'}),
-            'sub_county': forms.TextInput(attrs={'readonly': 'readonly'}),
-            'district': forms.TextInput(attrs={'readonly': 'readonly'}),
-            'occupation': forms.TextInput(attrs={'readonly': 'readonly'}),
-            'marital_status': forms.Select(attrs={'readonly': 'readonly'}),
-        }
+        model = PreviousObstetricHistory
+        fields = '__all__'
+        exclude = ['antenatal_card', 'recorded_by']
+
+class AntenatalProgressExaminationForm(forms.ModelForm):
+    class Meta:
+        model = AntenatalProgressExamination
+        fields = '__all__'
+        exclude = ['antenatal_card', 'recorded_by']
+
+class UltrasoundReportForm(forms.ModelForm):
+    class Meta:
+        model = UltrasoundReport
+        fields = '__all__'
+        exclude = ['antenatal_card', 'recorded_by']

474b0b41abb5affa5ea006e1ead358d00347b47f
allankalema4 <allankalema4@gmail.com>
2025-03-08 23:12:40 +0300

    timezone

--- health/settings.py
+++ health/settings.py
@@ -91,7 +91,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Kampala'
 USE_I18N = True
 USE_TZ = True
 

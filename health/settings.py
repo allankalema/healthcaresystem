933ef1fb67c6b4ca16b9eed176a66ef27dbfb68f
allankalema4 <allankalema4@gmail.com>
2025-03-13 10:27:18 +0300

    connected a local database to this system

--- health/settings.py
+++ health/settings.py
@@ -64,12 +64,18 @@ WSGI_APPLICATION = 'health.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # Now correctly references BASE_DIR
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'momcare',
+        'USER': 'django_user',
+        'PASSWORD': 'alan.KAL3',
+        'HOST': 'localhost',  # Change this when hosting
+        'PORT': '3306',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
